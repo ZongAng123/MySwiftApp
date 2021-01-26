ede3046fa00d68b8f939c0e1e9f6737202961717
纵昂 <zongang15@yahoo.com>
2021-01-26 15:50:35 +0800

    Swift初级代码

--- MySwiftApp/AppDelegate.swift
+++ MySwiftApp/AppDelegate.swift
@@ -10,26 +10,27 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+//        Swift自定义UITabBar
+//        创建Window
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.backgroundColor = UIColor.white
+//        初始化一个tabbar
+        let customTabBar = IWCustomTabBarController()
+//        设置跟视图控制器
+        window?.rootViewController = customTabBar
+       
+        window?.makeKeyAndVisible()
+        
+        
         return true
     }
 
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
+   
 
 
 }
---
+++ MySwiftApp/Class/My/MYViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MYViewController.swift
+//  MySwiftApp
+//
+//  Created by 纵昂 on 2021/1/26.
+//
+
+import UIKit
+
+class MYViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+        
+        
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
